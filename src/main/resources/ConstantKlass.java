b6298e757ebcaf886953e481ed9a996977017776
unknown <nikh0881@LJ21YGV1.RACKSPACE.CORP>
2012-10-06 15:03:34 -0500

    added new constant file into the repo

---
+++ src/main/resources/ConstantKlass.java
@@ -0,0 +1,17 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: nikh0881
+ * Date: 10/6/12
+ * Time: 2:22 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ConstantKlass {
+
+    public  final String SUB_MODULE_NAME ="Constant referred from submodule";
+    public final String SUB_MODULE_REPO="this is git submodule repo";
+
+
+
+
+
+}
